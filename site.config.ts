12c57bb51768b0d47172d79a3b1be42401f5c660
linyc <barney0817.cs10@nycu.edu.tw>
2022-12-29 21:32:32 +0800

    Add radis

--- site.config.ts
+++ site.config.ts
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'Barney Lin',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
+  domain: 'personal-linyc0817.vercel.app',
   author: 'Barney Lin',
 
   // open graph metadata (optional)
@@ -36,7 +36,7 @@ export default siteConfig({
   // whether or not redis is enabled for caching generated preview images (optional)
   // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
   // environment variables. see the readme for more info
-  isRedisEnabled: false,
+  isRedisEnabled: true,
 
   // map of notion page IDs to URL paths (optional)
   // any pages defined here will override their default URL paths

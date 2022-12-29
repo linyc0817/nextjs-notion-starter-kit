fb80f684628a954830b1dd47d9cf2c8e08bfac46
linyc <barney0817.cs10@nycu.edu.tw>
2022-12-29 21:10:25 +0800

    Update notion ID

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '884b24e972514ac48b8d0c373c4cac2',
+  rootNotionPageId: '884b24e972514ac48b8d0c373c4cac22',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
